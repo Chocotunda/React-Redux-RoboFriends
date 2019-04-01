3563f871ae4fbdc90d505e0053948dc5bd8885d6
KrishenG <kdgganpat@gmail.com>
2019-04-01 23:03:19 +0200

    Added ErrorBoundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,10 +2,8 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  // const cardComponent = robots.map((user, i) => { return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email} />})
   return (
     <div>
-      {/* {cardComponent} */}
       {robots.map((user, i) => {
         return (
           <Card
