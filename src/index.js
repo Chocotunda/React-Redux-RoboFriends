a4eba06f9d516ff78f67f427a56d39014027a1e2
Chocotunda <kdgganpat@gmail.com>
2019-04-01 20:52:58 +0200

    clean up and restructure files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './containers/App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
